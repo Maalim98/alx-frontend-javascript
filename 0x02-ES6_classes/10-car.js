1c1c4222d78c70997881a83814a1fa9f5162459a
Maalim98 <ashrafzani585@gmail.com>
2023-09-29 07:01:38 +0000

    all tasks

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this.prototype.constructor;
+  }
+
+  cloneCar() {
+    return new (this.constructor[Symbol.species])();
+  }
+}
